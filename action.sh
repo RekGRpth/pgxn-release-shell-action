34176dbba518d6fd5cab06d491b30d3bdfea8bb0
RekGRpth <rekgrpth@gmail.com>
2021-08-10 14:19:10 +0500

    curl

--- action.sh
+++ action.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 set -eux
+curl \
+    --form "archive=@${zipfile}" \
+    --form 'submit=Release It!' \
+    --header 'X-Requested-With: XMLHttpRequest' \
+    --user "${INPUTS_USERNAME}:${INPUTS_PASSWORD}" \
+    https://manager.pgxn.org/upload
