88879201428b527c29a2f43d8fd9ebc36846fecb
RekGRpth <rekgrpth@gmail.com>
2021-08-10 15:46:23 +0500

    up

--- action.sh
+++ action.sh
@@ -2,9 +2,5 @@
 
 set -eux
 curl --location --output "${GITHUB_REPOSITORY##*/}-${GITHUB_REF##*/}.zip" "${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/archive/refs/tags/${GITHUB_REF##*/}.zip"
-curl \
-    --form "archive=@${GITHUB_REPOSITORY##*/}-${GITHUB_REF##*/}.zip" \
-    --form 'submit=Release It!' \
-    --header 'X-Requested-With: XMLHttpRequest' \
-    --user "${INPUTS_USERNAME:-${GITHUB_REPOSITORY_OWNER}}:${INPUTS_PASSWORD}" \
-    https://manager.pgxn.org/upload
+STATUS="$(curl --form "archive=@${GITHUB_REPOSITORY##*/}-${GITHUB_REF##*/}.zip" --form 'submit=Release It!' --header 'X-Requested-With: XMLHttpRequest' --user "${INPUTS_USERNAME:-${GITHUB_REPOSITORY_OWNER}}:${INPUTS_PASSWORD}" --write-out "%{http_code}" https://manager.pgxn.org/upload)"
+test "${STATUS}" -ne 200 && exit 1
