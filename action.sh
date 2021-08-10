92436272d1c06ce60daa1b8c6adac5f3e651c0a1
RekGRpth <rekgrpth@gmail.com>
2021-08-10 14:30:24 +0500

    up

--- action.sh
+++ action.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 set -eux
+curl --output="${GITHUB_REF##*/}.zip" "https://github.com${GITHUB_REPOSITORY}/archive/refs/tags/${GITHUB_REF##*/}.zip"
 curl \
-    --form "archive=@${zipfile}" \
+    --form "archive=@${GITHUB_REF##*/}.zip" \
     --form 'submit=Release It!' \
     --header 'X-Requested-With: XMLHttpRequest' \
-    --user "${INPUTS_USERNAME}:${INPUTS_PASSWORD}" \
+    --user "${INPUTS_USERNAME:-${GITHUB_REPOSITORY_OWNER}}:${INPUTS_PASSWORD}" \
     https://manager.pgxn.org/upload
